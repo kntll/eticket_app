4e0a8e0490525dc9c2307dae7b8a3a27630d1a53
Lars <lars@knitel.nl>
2014-03-27 12:54:15 +0100

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,12 +5,39 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Eticket App'" do
-      visit '/static_pages#home'
-  	  expect(page).to have_title("Eticket App | Home")
+      visit '/home'
+      expect(page).to have_content('Eticket App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/home'
+      expect(page).to have_title("Eticket App | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/help'
+      expect(page).to have_title("Eticket App | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/about'
+      expect(page).to have_title("Eticket App | About Us")
     end
   end
 end
-
-
-
-
